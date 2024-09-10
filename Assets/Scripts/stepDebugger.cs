29e084a5c64aee5cb12ffb0277bc1ceb845c9864
xyxare <xyxar.estaniel-20@cpu.edu.ph>
2024-09-10 21:05:54 +0800

    <feature> Step count persists after app is closed

--- Assets/Scripts/stepDebugger.cs
+++ Assets/Scripts/stepDebugger.cs
@@ -2,12 +2,13 @@ using TMPro;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEngine.Android;
 using UnityEngine.InputSystem;
 
-
-public class StepDebugger : MonoBehaviour
+public class TrackerOfSteps : MonoBehaviour
+//THIS DOES WAY TOO MUCH STUFF REMIND ME TO REFACTOR THIS AT SPRINT'S END
 {
 
 
@@ -15,26 +16,65 @@ public class StepDebugger : MonoBehaviour
   [SerializeField]TMP_Text messageText, DEBUGTEXT;
 
    long stepOffset;
+   string stepJsonFilePath;
+   private int numberOfSteps;
+   private int loadedNumberOfSteps = 0;
+   private int sessionNumberOfSteps;
    void Start()
    {
+      messageText.text = "??????";
+      sessionNumberOfSteps = 0;
+       stepJsonFilePath = Application.persistentDataPath + "/stepData.json";
        if (Application.isEditor) { return; }
        RequestPermission(); 
        InputSystem.EnableDevice(StepCounter.current);
+       messageText.text = "Now tracking steps. If you're still seeing this, Xyxar fucked up.";
+       if (System.IO.File.Exists(stepJsonFilePath)){
+        
+        
+        loadStepData();
+         // loads the thing
+        updateStepsText(); // sets counter to the loaded step count
+
+       }
+       
    }
 
    void Update()
    {
        if (Application.isEditor) { return; }
-
+      sessionNumberOfSteps = StepCounter.current.stepCounter.ReadValue(); // 
+      numberOfSteps = sessionNumberOfSteps; // not += because it would go from +1 to +2 etc instead of +1 constant per step
        if (stepOffset == 0)
        {
-           stepOffset = StepCounter.current.stepCounter.ReadValue();
+           stepOffset = sessionNumberOfSteps;
            DEBUGTEXT.text = ("Step offset " + stepOffset);
        }
        else
        {
-           messageText.text = "Steps: " + (StepCounter.current.stepCounter.ReadValue() - stepOffset).ToString();
+           updateStepsText();
+           saveStepData();
        }
+   }
+
+   void updateStepsText(){
+    messageText.text = "Steps: " + (sessionNumberOfSteps + loadedNumberOfSteps).ToString();
+   }
+
+   void saveStepData(){
+    StepData data = new StepData();
+    data.numberOfSteps = numberOfSteps;
+    string stepCountString = JsonUtility.ToJson(data);
+
+
+    System.IO.File.WriteAllText(stepJsonFilePath, stepCountString);
+   }
+
+   void loadStepData(){
+    string stringCountJson = System.IO.File.ReadAllText(stepJsonFilePath);
+
+    loadedNumberOfSteps = JsonUtility.FromJson<StepData>(stringCountJson).numberOfSteps;
+    updateStepsText();
    }
 
 
@@ -44,11 +84,12 @@ public class StepDebugger : MonoBehaviour
            DEBUGTEXT.text = ("Editor Platform");
        #endif
        #if UNITY_ANDROID
-           AndroidRuntimePermissions.Permission result = await AndroidRuntimePermissions.RequestPermissionAsync("android.permission.ACTIVITY_RECOGNITION");
-           if (result == AndroidRuntimePermissions.Permission.Granted)
+           AndroidRuntimePermissions.Permission stepTrackerResult = await AndroidRuntimePermissions.RequestPermissionAsync("android.permission.ACTIVITY_RECOGNITION");
+           AndroidRuntimePermissions.Permission fileManagementResult = await AndroidRuntimePermissions.RequestPermissionAsync("android.permission.MANAGE_EXTERNAL_STORAGE");
+           if (stepTrackerResult == AndroidRuntimePermissions.Permission.Granted & fileManagementResult == AndroidRuntimePermissions.Permission.Granted)
                DEBUGTEXT.text = ("Step offset amount " + stepOffset);
            else
-            DEBUGTEXT.text = ("Permission state: " + result);
+            DEBUGTEXT.text = ("Permission state: " + stepTrackerResult);
        #endif
    }
 }
