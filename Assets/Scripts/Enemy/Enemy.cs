3e3fcf044aaf38ad38bee8f6dd62ff755397c70a
ferjen <ferjendavetorred@gmail.com>
2024-09-28 00:13:38 +0800

    Feat(Enemy Attack): added enemy attack state

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,7 @@
+﻿using KBCore.Refs;
 using UnityEngine;
 using UnityEngine.AI;
+using Utilities;
 
 namespace HomeByMarch
 {
@@ -11,18 +13,26 @@ namespace HomeByMarch
         [SerializeField] PlayerDetector playerDetector;
         [SerializeField] private NavMeshAgent agent;
         [SerializeField] private Animator animator;
+        [SerializeField] float timeBetweenAttack =1f;
 
         [SerializeField] float wanderRadius = 10f;
         private StateMachine stateMachine;
+
+        CountdownTimer attackTimer;
         void Start()
         {
+            attackTimer = new CountdownTimer(timeBetweenAttack);
             stateMachine = new StateMachine();
     
             var wanderState = new EnemyWanderState(this, animator, agent, wanderRadius);
             var chaseState = new EnemyChaseState(this, animator, agent, playerDetector.Player);
+            var attackState = new EnemyAttackState(this, animator, agent,playerDetector.Player);
             // Initialize other components if necessary
             At(wanderState, chaseState, new FuncPredicate(() => playerDetector.CanDetectPlayer()));
             At(chaseState, wanderState, new FuncPredicate(() => !playerDetector.CanDetectPlayer()));
+            At(chaseState, attackState, new FuncPredicate(() => playerDetector.CanAttackPlayer()));
+            At(attackState, chaseState, new FuncPredicate(() => !playerDetector.CanAttackPlayer()));
+             
 
             stateMachine.SetState(wanderState);
         }
@@ -35,11 +45,18 @@ namespace HomeByMarch
         void Update()
         {
             stateMachine.Update();
+            attackTimer.Tick(Time.deltaTime);
         }
 
         void FixedUpdate()
         {
             stateMachine.FixedUpdate();
+        }
+
+        public void Attack(){
+            if(attackTimer.IsRunning) return;
+            attackTimer.Start();
+            Debug.Log("Attacking");
         }
     }
 }
