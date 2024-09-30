f8e9b7e235c8e379ac585cfb538536afa695f0cc
ferjen <ferjendavetorred@gmail.com>
2024-09-30 20:13:42 +0800

    Fix(Step_Tracker): fixed the step tracking

---
+++ Assets/StepCounter/Demo/StepCountDemo.cs
@@ -0,0 +1,104 @@
+using Repforge.StepCounterPro;
+using System;
+using TMPro;
+using UnityEngine;
+
+public class StepCountDemo : MonoBehaviour
+{
+
+    public TMP_Text text;
+    public Canvas permissionCanvas;
+    void OnEnable(){
+        StepsToday();
+    }
+    void Start()
+    {
+        StepCounterRequest permissionRequest = new StepCounterRequest();
+        permissionRequest
+            .OnPermissionGranted(OnPermissionGranted)
+            .OnPermissionDenied(OnPermissionDenied)
+            .RequestPermission();
+    }
+
+    private void OnPermissionGranted()
+    {
+        StepCounterRequest stepRequest = new StepCounterRequest();
+        stepRequest
+            .Since(DateTime.Today)
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .Execute();
+    }
+
+    private void OnPermissionDenied()
+    {
+        permissionCanvas.gameObject.SetActive(true);
+    }
+
+    public void OpenAppSettings()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request.OpenAppSettings();
+    }
+
+    public void StepsBetween5and10MinutesAgo()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Between(DateTime.Now.AddMinutes(-10), DateTime.Now.AddMinutes(-5))
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+    public void StepsBetween9and11amToday()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Between(DateTime.Today.AddHours(9), DateTime.Today.AddHours(11))
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+    public void StepsPast5Minutes()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Since(DateTime.Now.AddMinutes(-5))
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+    public void StepsPastHour()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Since(DateTime.Now.AddHours(-1))
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+    public void StepsToday()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Since(DateTime.Today)
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+    public void StepsYesterday()
+    {
+        StepCounterRequest request = new StepCounterRequest();
+        request
+            .Between(DateTime.Today.AddDays(-1), DateTime.Today)
+            .OnQuerySuccess((value) => text.text = value.ToString())
+            .OnPermissionDenied(() => permissionCanvas.gameObject.SetActive(true))
+            .Execute();
+    }
+
+
+}
